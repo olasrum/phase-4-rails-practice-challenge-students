8651f5159b3aaf4f2421e41065413209fe5107c7
olasrum <mursalo@gmail.com>
2023-02-22 15:41:36 +1000

    Done with assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,50 @@
+class StudentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def show
+        student = find_student
+        render json: student
+    end
+
+    def create
+        student = Student.create!(student_params)
+        render json: student, status: :created
+    end
+
+    def update
+        student = find_student
+        student.update!(student_params)
+        render json: student, status: :accepted
+    end
+
+    def destroy
+        student = find_student
+        student.destroy
+        head :no_content
+    end
+
+
+    private
+
+    def find_student
+        Student.find(params[:id])
+    end
+
+    def student_params
+        params.permit(:name, :major, :age)
+    end
+
+    def render_record_not_found_response
+        render json: {error: "Student not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+end
